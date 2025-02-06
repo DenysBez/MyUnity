b0985f2420a7352596ff9f1ee492ddc4d5ed3fce
DenysBez <denysbez.fi@gmail.com>
2025-02-06 14:37:23 +0200

    first commit

---
+++ Assets/Code/EmptyGameObject.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptyGameObject : MonoBehaviour
+{
+    void Start()
+    {
+        int health = 100;
+        float speed = 5.5f;
+        bool isAlive = true;
+
+        Debug.Log($"Health: {health} (int)");
+        Debug.Log($"Speed: {speed} (float)");
+        Debug.Log($"Is Alive: {isAlive} (bool)");
+        
+        DamageSystem damageSystem = new DamageSystem(50, 1.2f);
+        float damage = damageSystem.CalculateDamage();
+
+        Debug.Log($"Total Damage Calculated: {damage}");
+    }
+}
+
+// Damage calculation class
+public class DamageSystem
+{
+    private int baseDamage;
+    private float multiplier;
+
+    public DamageSystem(int baseDamage, float multiplier)
+    {
+        this.baseDamage = baseDamage;
+        this.multiplier = multiplier;
+    }
+
+    public float CalculateDamage()
+    {
+        float totalDamage = baseDamage * multiplier;
+        Debug.Log($"Calculating Damage: {baseDamage} * {multiplier} = {totalDamage}");
+        return totalDamage;
+    }
+}
---
+++ Assets/Code/SphereCharacter.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class SphereCharacter : MonoBehaviour
+{
+    public int hp = 100;
+    public bool isDeath = false;
+    public bool isEnemy = false;
+
+    private MeshRenderer meshRenderer;
+
+    void Start()
+    {
+        
+        meshRenderer = GetComponent<MeshRenderer>();
+
+        
+        if (isEnemy)
+        {
+            meshRenderer.material.color = Color.red;
+            Debug.Log("Enemy, Color Red");
+        }
+        else
+        {
+            meshRenderer.material.color = Color.blue;
+            Debug.Log("Ally, Color Blue");
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (isDeath)
+        {
+            return;
+        }
+        hp -= damage;
+
+        if (hp <= 0)
+        {
+            hp = 0; 
+            isDeath = true;
+            Debug.Log("Deth");
+        }
+        else
+        {
+            Debug.Log($"Damage {damage}. HP Left: {hp}");
+        }
+    }
+}
